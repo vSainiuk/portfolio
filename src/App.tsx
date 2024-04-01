b62c00fc640b0e0116bf68bf5b97f64f78a257e3
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-04-01 18:14:01 +0300

    started project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Home } from './Components/Home/Home.tsx';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main className="App">
+      <Home />
+    </main>
+  );
+};
+
+export default App;
