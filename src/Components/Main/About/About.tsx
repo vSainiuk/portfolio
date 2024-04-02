6c3237e22ce83d2c65332834065295d955a49958
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-04-02 17:48:23 +0300

    added about block

---
+++ src/Components/Main/About/About.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+
+import './About.scss';
+
+export const About: React.FC = () => {
+  return (
+    <Card sx={{ display: 'flex', marginTop: '300px', height: '370px' }}>
+      <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
+        <div className="about-image" />
+      </CardContent>
+      <CardContent sx={{ textAlign: 'left'}}>
+        <Typography style={{fontWeight: 'bold'}} className='title' variant="h5">
+          About Me
+        </Typography>
+        <Typography
+          style={{ marginTop: '20px', fontWeight: 'bold' }}
+          variant="h5"
+        >
+          I am a Full-Stack Developer with a passion for creating web
+          applications.
+        </Typography>
+        <Typography style={{ marginTop: '20px' }} variant="body1">
+          As a full-stack developer, I'm focused on building innovative products
+          using React that not only solve problems, but also bring significant
+          value to users and society. My commitment to continuous
+          self-improvement and learning allows me to stay one step ahead in the
+          fast-paced world of technology. I like to take on new projects that
+          challenge me and stimulate the development of my skills. I believe
+          that learning never ends and I am always on the lookout for new
+          opportunities to grow.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Home } from './Home/Home.tsx';
+import { About } from './About/About.tsx';
 
 export const Main: React.FC = () => {
   return (
     <div className='container'>
       <Home />
+      <About />
     </div> 
   );
 };
