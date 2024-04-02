d30b9db34acc37ee360b572b37733c560adcbafb
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-04-03 01:35:23 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Header } from './Components/Header/Header.tsx';
 import { Main } from './Components/Main/Main.tsx';
 import './App.scss';
+import { Footer } from './Components/Footer/Footer.tsx';
 
 const App = () => {
   return (
     <main className="App">
       <Header />
       <Main />
+      <Footer />
     </main>
   );
 };
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { CardContent } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      justifyContent: 'space-between',
+      paddingInline: '300px',
+      backgroundColor: 'black',
+      color: 'white',
+    },
+  })
+);
+
+export const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.root}>
+        <Typography style={{fontWeight: 'bold'}} variant="h6"> Copyright &copy; {new Date().getFullYear()} All rights reserved</Typography>
+        <CardContent sx={{ display: 'flex', gap: '20px' }}>
+              <a className='link-home' href="https://www.linkedin.com/in/vadym-sainiuk-552104301/" rel="noreferrer" target="_blank">
+                <LinkedInIcon />
+              </a>
+              <a className='link-home' href="https://github.com/vSainiuk" rel="noreferrer" target="_blank">
+                <GitHubIcon />
+              </a>
+            </CardContent>
+      </Toolbar>
+    </AppBar>
+  );
+}
